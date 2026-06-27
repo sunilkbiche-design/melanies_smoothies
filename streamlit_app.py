c9fd21611470beed0885580aecc25a9dc9546234
sunilkbiche-design <sunil.k.biche@kipi.ai>
2026-06-28 02:51:03 +0530

    Update streamlit_app.py
    Request Library and DF is added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+# New section to display smoothiefruit nutrition information
+
 import requests
+
+smoothiefruit_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+
+# st.text(smoothiefruit_response.json())
+
+sf_df = st.dataframe(
+    data=smoothiefruit_response.json(),
+    use_container_width=True
+)
 
 from snowflake.snowpark.functions import col
 
